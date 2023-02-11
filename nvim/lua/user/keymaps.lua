573a4e1d158a8cf191fe36aa73b765a2b16e336c
Mohammed Taher <mohammed.taher2007@gmail.com>
2023-02-11 22:03:37 +0300

    Added Git integration

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true})
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true})
+
+-- Reselect visual selection after indenting.
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Maintain the cursor position when yanking a visual selection.
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+vim.keymap.set('v', 'y', 'myy`y')
+
+-- Paste replace visual selection without copying it.
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Quickly clear search highlighting.
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
+
+-- Open the current file in the default program (on Mac this should just be just `open`).
+vim.keymap.set('n', '<Leader>x', ':!open %<CR><CR>')
+
---
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -0,0 +1,39 @@
+local dashboard = require('dashboard')
+
+dashboard.setup({
+    theme = 'hyper',
+    config = {
+      week_header = {
+       enable = true,
+      },
+      shortcut = {
+        { desc = ' Update', group = '@property', action = 'PackerSync', key = 'u' },
+        {
+          icon = ' ',
+          icon_hl = '@variable',
+          desc = 'Files',
+          group = 'Label',
+          action = 'Telescope find_files',
+          key = 'f',
+        },
+        {
+          desc = ' Apps',
+          group = 'DiagnosticHint',
+          action = 'Telescope app',
+          key = 'a',
+        },
+        {
+          desc = ' dotfiles',
+          group = 'Number',
+          action = 'Telescope dotfiles',
+          key = 'd',
+        },
+      },
+    },
+  })
+
+vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#6272a4' })
+vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = '#f8f8f2' })
+vim.api.nvim_set_hl(0, 'DashboardShortcut', { fg = '#bd93f9' })
+vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#6272a4' })
+
